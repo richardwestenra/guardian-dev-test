c92b0b1e1114e3af1d332e30c5a60b3dd01ba5f1
Richard Westenra <rjwestenra@gmail.com>
2015-12-10 20:59:47 +0000

    Build and style basic tabs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,31 +5,75 @@
     sections = ['uk-news', 'football', 'travel'];
 
   var postsTemplate = document.getElementById('tabs-posts-template').innerHTML,
-    tabs = document.getElementById('tabs');
+    tabs = document.getElementById('tabs'),
+    tabsContent = document.getElementById('tabs-content');
 
 
   // Initialise requests
   get( url(sections[0]) )
-  .then(function(response) {
-    makeList(0, JSON.parse(response) );
-    return get( url(sections[1]) );
-  })
-  .then(function(response) {
-    makeList(1, JSON.parse(response) );
-    return get( url(sections[2]) );
-  })
-  .then(function(response) {
-    makeList(2, JSON.parse(response) );
-  })
-  .catch(function(error) {
-    console.error(error);
+    .then(function(response) {
+      makeList(0, JSON.parse(response) );
+      return get( url(sections[1]) );
+    })
+    .then(function(response) {
+      makeList(1, JSON.parse(response) );
+      return get( url(sections[2]) );
+    })
+    .then(function(response) {
+      makeList(2, JSON.parse(response) );
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+
+
+  // Add tab click event listener
+  [].forEach.call(tabs.querySelectorAll('a'), (el) => {
+    el.addEventListener('click', function (e){
+
+      var target = el.getAttribute('href'),
+        id = +target.substr(-1);
+
+      toggleTabs( el.parentNode );
+
+      toggleTabContent( target.substr(1) );
+
+      // Check for new posts
+      get( url(sections[id]) )
+        .then(function(response) {
+          makeList(id, JSON.parse(response) );
+        })
+        .catch(function(error) {
+          console.error(error);
+        });
+
+      e.preventDefault();
+
+    }, false);
   });
 
 
+  function toggleTabs(el) {
+    // Toggle tabs
+    Array.prototype.filter
+      .call(tabs.querySelectorAll('li'), (li) => li !== el)
+      .forEach((li) => {
+        li.setAttribute('aria-selected', false);
+        li.classList.remove('tabs__tab--selected');
+      });
 
-  tabs.querySelectorAll('a').addEventListener('click', function (e){
-    e.preventDefault();
-  }, false);
+    el.setAttribute('aria-selected', true);
+    el.classList.add('tabs__tab--selected');
+  }
+
+
+  function toggleTabContent(id) {
+    Array.prototype.forEach
+      .call(tabsContent.querySelectorAll('.tabs__panel'), (panel) => {
+        var matches = panel.id === id;
+        panel.style.display = matches ? '' : 'none';
+      });
+  }
 
 
 
